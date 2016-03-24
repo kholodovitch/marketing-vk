a4d0ce4e2ebc56186a140c0b676c8c8003e172da
Alexander Kholodovitch <kholodovitch@gmail.com>
2016-03-24 09:58:02 +0300

    Allow CI-build publish only master branch.

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-curl "http://$1:$2@$3?path=/marketing-vk&update=true" --upload-file target/marketing-vk.war
+if [ "${TRAVIS_BRANCH}" = "master" ]; then
+	curl "http://$1:$2@$3?path=/marketing-vk&update=true" --upload-file target/marketing-vk.war
+fi
